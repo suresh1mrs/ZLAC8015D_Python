426bbbca88e98181f2c5c8152327356191fcb3ea
suresh1mrs <vaibhavshimewar00@gmail.com>
2023-09-11 01:02:39 +0530

    update current, voltage and temperature functions in ZLA library

--- examples/test_read.py
+++ examples/test_read.py
@@ -19,10 +19,15 @@ while True:
 	try:
 		# motors.set_rpm(cmds[0],cmds[1])
 		l_tick, r_tick = motors.get_wheels_tick()
-
 		print("period: {:.4f} l_tick: {:.1f} | r_tick: {:.1f}".format(period,l_tick,r_tick))
+		l_curr, r_curr = motors.get_motor_current()
+		print("period: {:.4f} l_curr: {:.1f} | r_curr: {:.1f}".format(period,l_curr,r_curr))
+		voltage = motors.get_driver_voltage()
+		print("period: {:.4f} voltage: {:.1f} ".format(period,voltage))
+		l_temp, r_temp = motors.get_motor_temperature()
+		print("period: {:.4f} l_temp: {:.1f} | r_temp: {:.1f}".format(period,l_temp,r_temp))
 		period = time.time() - last_time
-		time.sleep(0.01)
+		time.sleep(2)
 			
 
 	except KeyboardInterrupt:
